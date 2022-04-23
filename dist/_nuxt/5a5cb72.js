56d924fc1c1d6f769719385b3b4f5bc04fcdded3
Arbaaz <arbaazdurrani7@gmail.com>
2022-04-22 22:30:38 -0400

    add dist folder and its files

---
+++ dist/_nuxt/5a5cb72.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{284:function(e,n,t){"use strict";t.r(n);var o={components:{Home:t(282).default}},l=t(18),component=Object(l.a)(o,(function(){var e=this.$createElement;return(this._self._c||e)("Home")}),[],!1,null,null,null);n.default=component.exports}}]);
